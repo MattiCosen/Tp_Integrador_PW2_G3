a0cc766afcd565cd8ab87f2afd7a656ab68c221d
Aldo <aldocape@gmail.com>
2021-06-04 05:50:37 -0300

    Guardo datos de conexion en config.js

---
+++ src/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  PORT: 3000,
+  JWT_SECRET: "Esta clave es super secreta y tengo que asegurarla",
+  DB_CONFIG: {
+    host: "localhost",
+    user: "admin",
+    password: "password",
+    database: "grupo3",
+  },
+};
