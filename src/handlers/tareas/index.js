2685f1ba2754980ac45ac80700018590b3814487
Aldo <aldocape@gmail.com>
2021-06-04 08:53:12 -0300

    agrego endpoint show a tareas

--- src/handlers/tareas/index.js
+++ src/handlers/tareas/index.js
@@ -3,8 +3,10 @@ const express = require("express");
 const tareasRouting = express.Router();
 
 const list = require("./list");
+const show = require("./show");
 
 list(tareasRouting);
+show(tareasRouting);
 
 const tareasAPI = express.Router();
 
